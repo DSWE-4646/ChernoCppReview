fc7fc188a107127004b722977127e9089a8f2893
SiberianSledCat <dy464697573@gmail.com>
2025-12-05 14:28:04 +0800

    const in C++

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,15 +1,35 @@
 #include<iostream>
 //#include<string>
 
+class Entity
+{
+	mutable int m_X, m_Y;
+
+public:
+	int GetX() const
+	{
+		std::cout << m_X << std::endl;
+		return m_X;
+	}
+
+	void SetX(int x) const
+	{
+		m_X = x;
+	}
+};
+
+void PrintrEntityX(const Entity& e)
+{
+	//e = nullptr;
+	e.SetX(3);
+	e.GetX();
+	//e.GetX();
+	std::cout<<e.GetX();
+}
+
 int main()
 {
-	const int MAX_AGE = 90;
-
-	int* const a = new int;
-
-	*a = 2;
-	a = (int*)&MAX_AGE; // This is bad practice and leads to undefined behavior
-	std::cout << *a << std::endl;
+	Entity e;
 
 	std::cin.get();
 }
