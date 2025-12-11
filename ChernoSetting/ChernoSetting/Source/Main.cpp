9769c33dc6d961535e4a462af5ead64223968259
SiberianSledCat <dy464697573@gmail.com>
2025-12-11 10:25:38 +0800

    this

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,62 +1,38 @@
 #include<iostream>
 #include<string>
 
-struct vector2
+struct Entity
 {
-	float x, y;
-	vector2() : x(0), y(0) {}
-	vector2(float x, float y) : x(x), y(y) {}
+	int x, y;
 
-	vector2 Add(const vector2& other) const
+	Entity(int x, int y)
 	{
-		return vector2(x + other.x, y + other.y);
+		//x = x;
+		Entity* e = this;
+		this->x = x;
+		e->x = x;
+		(*this).x = x;
+
+		PrintEntity(*this);
+
+		delete this;
 	}
 
-	vector2 Multiply(const vector2& other) const
+	int GetX() const 
 	{
-		return vector2(x * other.x, y * other.y);
+		const Entity* e = this;
 	}
-
-	vector2 operator+(const vector2& other) const
-	{
-		return Add(other);
-		//return Vector2(x + other.x, y + other.y);
-	}
-
-	vector2 operator*(const vector2& other) const
-	{
-		return Multiply(other);
-		//return Vector2(x + other.x, y + other.y);
-	}
-
-	bool operator==(const vector2& other) const {
-		return x == other.x && y == other.y;
-	}
-
 };
 
-std::ostream& operator <<(std::ostream& stream, const vector2& vector)
+void PrintEntity(const Entity& e)
 {
-	stream << "{" << vector.x << ", " << vector.y << "}";
-	return stream;
+
 }
 
-
-bool operator!=(const vector2& left, const vector2& right)
-{
-	return !(left == right);
-}
 
 
 int main()
 {
-	vector2 position(5.0f, 10.0f);
-	vector2 speed(2.0f, 2.0f);
-	vector2 powerup(1.5f, 1.5f);
-
-	vector2 result = position + speed * powerup; // This line will cause a compilation error
-
-	std::cout << result << std::endl;
-
+	
 	std::cin.get();
 }
