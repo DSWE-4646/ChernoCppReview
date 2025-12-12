9f201efb1a0cdcf1355be84c5bcb073cf7dcd785
SiberianSledCat <dy464697573@gmail.com>
2025-12-12 14:02:28 +0800

    复制与拷贝构造函数

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -11,9 +11,8 @@ public:
 	String(const char* string)
 	{
 		m_Size = strlen(string);
-		//m_Buffer = new char[m_Size + 1];
-		m_Buffer = new char[m_Size];
-		memcpy(m_Buffer, string, m_Size);
+		m_Buffer = new char[m_Size + 1];
+		memcpy(m_Buffer, string, m_Size + 1);
 		//m_Buffer[m_Size] = 0;
 	}
 	friend std::ostream& operator<<(std::ostream& stream, const String& string);
@@ -28,14 +27,7 @@ public:
 		delete[] m_Buffer;
 	}
 
-	String(const String& other)
-	{
-		m_Size = other.m_Size;
-		//m_Buffer = new char[m_Size + 1];
-		m_Buffer = new char[m_Size];
-		memcpy(m_Buffer, other.m_Buffer, m_Size);
-		//m_Buffer[m_Size] = 0;
-	}
+	String(const String& other) : m_Buffer((other.m_Buffer)), m_Size(other.m_Size) {};
 };
 
 std::ostream& operator<<(std::ostream& stream, const String& string)
@@ -50,7 +42,6 @@ int main()
 	String string2 = string;
 
 	string2[2] = 'k';
-
 	std::cout << string << std::endl;
 	std::cout << string2 << std::endl;
 	
