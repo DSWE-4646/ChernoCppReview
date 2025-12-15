faab275dc6faab671f1cb823ad67df6a317b461f
SiberianSledCat <dy464697573@gmail.com>
2025-12-15 15:22:49 +0800

    STL与动态数组

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,17 +1,35 @@
 #include<iostream>
 #include<string>
-#include<memory>
+#include<vector>
 
-struct VectorSample
+struct Vertex
 {
-	int x, y, z;
-	long long int i;
+	float x, y, z;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Vertex& ver)
+{
+	stream << ver.x << "," << ver.y << "," << ver.z << std::endl;
+	return stream;
+}
 
 int main()
 {
-	int offset = (int)&((VectorSample*)0)->i;
-	std::cout<< offset<< std::endl;
+	std::vector <Vertex> vertices = { {1, 2, 3}, { 4, 5, 6 }, { 7, 8, 9 } };
+	//std::vector <Vertex> vertices;
+	
+	for(int i = 0; i < vertices.size(); i++)
+	std::cout<< vertices[i]<< std::endl;
+	vertices.clear();
+
+	vertices.push_back({ 3, 2, 1 });
+	vertices.push_back({ 6, 5, 4 });
+	vertices.push_back({ 9, 8, 7 });
+
+	vertices.erase(vertices.begin() + 1);
+
+	for(const Vertex& v : vertices)
+	std::cout << v << std::endl;
+
 	std::cin.get();
 }
