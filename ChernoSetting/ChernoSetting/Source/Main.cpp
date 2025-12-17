91153a0597161932a087ee38c728e12423727c4a
SiberianSledCat <dy464697573@gmail.com>
2025-12-17 15:02:00 +0800

    使用静态库

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,35 +1,10 @@
 #include<iostream>
-#include<string>
-#include<vector>
 
-struct Vertex
-{
-	float x, y, z;
-
-	Vertex(float x, float y, float z)
-		: x(x), y(y), z(z)
-	{
-	}
-
-	Vertex(const Vertex& vertex)
-		: x(vertex.x), y(vertex.y), z(vertex.z)
-	{
-		std::cout << "Copied!" << std::endl;
-	}
-};
-
+extern "C" int glfwInit();
 
 int main()
 {
-
-	std::vector <Vertex> vertices;
-
-	vertices.emplace_back( 3, 2, 1 );
-	vertices.emplace_back({ 6, 5, 4 });
-	
-	vertices.push_back({ 3, 2, 1 });
-	vertices.push_back({ 6, 5, 4 });
-	vertices.push_back({ 9, 8, 7 });
-
+	int a = glfwInit();
+	std::cout << a << std::endl;
 	std::cin.get();
 }
