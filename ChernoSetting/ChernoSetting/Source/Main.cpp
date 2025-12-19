94a310f75b34642f7d2a4306d3f28518c244e498
SiberianSledCat <dy464697573@gmail.com>
2025-12-19 15:02:59 +0800

    std::pair（仅支持 2 个变量）返回不同类型

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,21 +1,20 @@
 #include<iostream>
-#include<vector>
+#include<utility>
 #include<string>
 
-std::vector<std::string> pass_shader()
+std::pair<std::string, std::string> pass_shader()
 {
-	std::vector<std::string> shaders(2);
-	shaders[0] ="顶点着色器代码";
-	shaders[1] = "片段着色器代码";
-	
-	return shaders;
+	std::string vs = "顶点着色器代码";
+	std::string fs = "片段着色器代码";
+
+	return std::make_pair(vs, fs);
 }
 
 int main()
 {
 	//auto [vertex_shader_code, fragment_shader_code] = pass_shader();  // C++17 structured binding
 	auto shaders = pass_shader(); // C++14 compatible
-	std::cout << "Vertex Shader Code: " << shaders[0] << std::endl;
-	std::cout << "Fragment Shader Code: " << shaders[1] << std::endl;
+	std::cout << "Vertex Shader Code: " << shaders.first << std::endl;
+	std::cout << "Fragment Shader Code: " << shaders.second << std::endl;
 	return 0;
 }
