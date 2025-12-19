7fe5f593e96782600de9fccc1164ed64dc33a928
SiberianSledCat <dy464697573@gmail.com>
2025-12-19 15:00:39 +0800

    返回 std::vector（同类型多返回值，动态大小）

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
-#include<array>
+#include<vector>
 #include<string>
 
-std::array<std::string, 2> pass_shader()
+std::vector<std::string> pass_shader()
 {
-	std::string vshader = "顶点着色器代码";
-	std::string fshader = "片段着色器代码";
+	std::vector<std::string> shaders(2);
+	shaders[0] ="顶点着色器代码";
+	shaders[1] = "片段着色器代码";
 	
-	return { vshader, fshader };
+	return shaders;
 }
 
 int main()
@@ -15,6 +16,6 @@ int main()
 	//auto [vertex_shader_code, fragment_shader_code] = pass_shader();  // C++17 structured binding
 	auto shaders = pass_shader(); // C++14 compatible
 	std::cout << "Vertex Shader Code: " << shaders[0] << std::endl;
-	std::cout << "Fragment Shader Code: " << shaders[0] << std::endl;
+	std::cout << "Fragment Shader Code: " << shaders[1] << std::endl;
 	return 0;
 }
