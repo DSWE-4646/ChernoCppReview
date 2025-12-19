64d028f12576081b803232dbb082940f015b83c5
SiberianSledCat <dy464697573@gmail.com>
2025-12-19 15:05:16 +0800

    std::tuple（支持任意数量、任意类型变量）

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
-#include<utility>
+#include<tuple>
 #include<string>
 
-std::pair<std::string, std::string> pass_shader()
+std::tuple<std::string, std::string, int> pass_shader()
 {
 	std::string vs = "顶点着色器代码";
 	std::string fs = "片段着色器代码";
+	int version = 330;
 
-	return std::make_pair(vs, fs);
+	return std::make_tuple(vs, fs, version);
 }
 
 int main()
 {
 	//auto [vertex_shader_code, fragment_shader_code] = pass_shader();  // C++17 structured binding
 	auto shaders = pass_shader(); // C++14 compatible
-	std::cout << "Vertex Shader Code: " << shaders.first << std::endl;
-	std::cout << "Fragment Shader Code: " << shaders.second << std::endl;
+	std::cout << "Vertex Shader Code: " << std::get<0>(shaders) << std::endl;
+	std::cout << "Fragment Shader Code: " << std::get<1>(shaders) << std::endl;
+	std::cout << "Version ist: " << std::get<2>(shaders) << std::endl;
 	return 0;
 }
