c7e2b8cf000c8e1046dc521eeba9b646760d5423
SiberianSledCat <dy464697573@gmail.com>
2025-12-19 15:13:21 +0800

    自定义结构体（语义化首选,形式不必拘泥，可以像UE一样传入结构体应用，也可以函数内定义结构体return)

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -2,21 +2,26 @@
 #include<tuple>
 #include<string>
 
-std::tuple<std::string, std::string, int> pass_shader()
+struct ShaderData
 {
-	std::string vs = "顶点着色器代码";
-	std::string fs = "片段着色器代码";
-	int version = 330;
+	std::string vertex_shader;
+	std::string fragment_shader;
+};
 
-	return std::make_tuple(vs, fs, version);
+
+ShaderData pass_shader()
+{
+	ShaderData Shader;
+	Shader.vertex_shader = "顶点着色器代码";
+	Shader.fragment_shader = "片段着色器代码";
+	return Shader;
 }
 
 int main()
 {
 	//auto [vertex_shader_code, fragment_shader_code] = pass_shader();  // C++17 structured binding
 	auto shaders = pass_shader(); // C++14 compatible
-	std::cout << "Vertex Shader Code: " << std::get<0>(shaders) << std::endl;
-	std::cout << "Fragment Shader Code: " << std::get<1>(shaders) << std::endl;
-	std::cout << "Version ist: " << std::get<2>(shaders) << std::endl;
+	std::cout << "Vertex Shader Code: " << shaders.vertex_shader << std::endl;
+	std::cout << "Fragment Shader Code: " << shaders.fragment_shader << std::endl;
 	return 0;
 }
