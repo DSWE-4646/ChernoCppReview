500acad77c26948c0a89153686966710f42647a0
SiberianSledCat <dy464697573@gmail.com>
2025-12-19 14:19:04 +0800

    指针参数（输出参数，带 out 前缀）返回相同类型

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 
-void pass_shader(std::string& vertex_source, std::string& fragment_source)
+void pass_shader(std::string* out_vertex_source, std::string* out_fragment_source)
 {
 	std::string vshader = "顶点着色器代码";
 	std::string fshader = "片段着色器代码";
 	
-	vertex_source = vshader;
-	fragment_source = fshader;
+	*out_vertex_source = vshader;
+	*out_fragment_source = fshader;
 }
 
 int main()
 {
 	std::string vertex_shader_code;
 	std::string fragment_shader_code;
-	pass_shader(vertex_shader_code, fragment_shader_code);
+	pass_shader(&vertex_shader_code, &fragment_shader_code);
 	std::cout << "Vertex Shader Code: " << vertex_shader_code << std::endl;
 	std::cout << "Fragment Shader Code: " << fragment_shader_code << std::endl;
 	return 0;
