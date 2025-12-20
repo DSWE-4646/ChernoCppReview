415bf794069be927554049c6654d9ec9dabed122
SiberianSledCat <dy464697573@gmail.com>
2025-12-20 11:33:26 +0800

    使用模板指定栈上的静态数组大小

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,27 +1,20 @@
 #include<iostream>
-#include<tuple>
+#include<array>
 #include<string>
 
-struct ShaderData
+template<int N>
+class Array
 {
-	std::string vertex_shader;
-	std::string fragment_shader;
+private:
+	int m_Array[N];
+public:
+	int GetSize() const { return N; }
 };
 
 
-ShaderData pass_shader()
-{
-	ShaderData Shader;
-	Shader.vertex_shader = "顶点着色器代码";
-	Shader.fragment_shader = "片段着色器代码";
-	return Shader;
-}
-
 int main()
 {
-	//auto [vertex_shader_code, fragment_shader_code] = pass_shader();  // C++17 structured binding
-	auto shaders = pass_shader(); // C++14 compatible
-	std::cout << "Vertex Shader Code: " << shaders.vertex_shader << std::endl;
-	std::cout << "Fragment Shader Code: " << shaders.fragment_shader << std::endl;
+	Array<5> a;
+	std::cout << "Array size: " << a.GetSize() << std::endl;
 	return 0;
 }
