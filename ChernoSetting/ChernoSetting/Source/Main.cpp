6216fc0791526eea46b92573c1dd6c4c1aea958c
SiberianSledCat <dy464697573@gmail.com>
2025-12-24 11:34:20 +0800

    array基础用法

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-
-
-#define PRINT(x) std::cout<< x<<std::endl
-#define WAIT std::cin.get()
-
-
-#if PRE_DEBUG  ==  1
-	#define LOG(x) std::cout<< x<<std::endl
-#else 
-	#define LOG(x) 
-#endif
+#include<array>
 
 int main() {
-	LOG("This is a log");
-	WAIT;
+	std::array<int, 5> arr = { 1, 2, 3, 4, 5 };
+	std::array<int, 5> arr2;
+	arr2.fill(0);
+
+	std::cout << arr[3] << std::endl;
+	std::cout << arr2.at(3) << std::endl;
+
+	std::cout << "Size of arr: " << arr.size() << std::endl;
+	std::cout << "Max Size of arr2: " << arr2.max_size() << std::endl;
+	std::cout << "Is Empty Of array:" << arr.empty() << std::endl;
+
+	const int* p = arr.data();
+	std::cout << "First Element of arr using data(): " << p[3] << std::endl;
+
 }
