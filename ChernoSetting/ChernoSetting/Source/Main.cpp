506cd6fdea4c235c39325c08ad1f594b35c6b2af
SiberianSledCat <dy464697573@gmail.com>
2025-12-25 11:37:01 +0800

    array的STL特性与成员函数

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include<array>
+#include <algorithm>
+
+void printArray(const std::array<int, 5>& a) {
+	for (size_t i = 0; i < a.size(); i++) {
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
 int main() {
-	std::array<int, 5> arr = { 1, 2, 3, 4, 5 };
-	std::array<int, 5> arr2;
-	arr2.fill(0);
+	std::array<int, 5> arr = { 7, 2, 5, 9, 3 };
+	std::array<int, 3> arr2 = { 1, 2, 3};
 
-	std::cout << arr[3] << std::endl;
-	std::cout << arr2.at(3) << std::endl;
-
-	std::cout << "Size of arr: " << arr.size() << std::endl;
-	std::cout << "Max Size of arr2: " << arr2.max_size() << std::endl;
-	std::cout << "Is Empty Of array:" << arr.empty() << std::endl;
-
-	const int* p = arr.data();
-	std::cout << "First Element of arr using data(): " << p[3] << std::endl;
+	for (auto var : arr) {
+		std::cout << var << std::endl;
+	}
+	
 
 }
