42e2a80d4cb62b551e0ae60887dae9eb9921202a
SiberianSledCat <dy464697573@gmail.com>
2025-12-26 12:01:59 +0800

    lambda算法谓语

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
-
-
-
-int main()
-{
-    int a = 5;
-	int b = 10;
-
-    auto lambda1 = [=]() {
-        //a = 7;
-        std::cout << "Lambda1: a = " << a << ", b = " << b << std::endl;
-		};
-
-    lambda1();
-
-    auto lambda2 = [&]() {
-        a = 7;
-        b = 12;
-        std::cout << "Lambda2: a = " << a << ", b = " << b << std::endl;
-        };
-	lambda2();
-
-    auto lambda3 = [a, &b]() mutable {
-        a = 20;
-        b = 25;
-        std::cout << "Lambda3: a = " << a << ", b = " << b << std::endl;
-		};
-	lambda3(); 
+#include <vector>
+#include <algorithm> // std::find_if头文件
+int main() {
+    std::vector<int> values = { 1, 0, 4, 9, 3 };
+    // std::find_if：查找第一个满足条件的元素，Lambda作为谓词
+    auto it = std::find_if(values.begin(), values.end(), [](int val) {
+        return val > 3; // 条件：元素大于3
+        });
+    if (it != values.end()) {
+        std::cout << "第一个大于3的元素：" << *it << std::endl; // 输出：5
+    }
     return 0;
 }
