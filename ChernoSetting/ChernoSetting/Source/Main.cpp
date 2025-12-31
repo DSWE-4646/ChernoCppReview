1bbe67c1ee369805f8a2a4b83739e4fb822f897e
SiberianSledCat <dy464697573@gmail.com>
2025-12-31 14:06:38 +0800

    使用构造-析构的计时器

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -2,12 +2,34 @@
 #include<thread>
 #include<chrono>
 
+struct Timer
+{
+	Timer()
+	{
+		start = std::chrono::high_resolution_clock::now();
+	}
+	~Timer()
+	{
+		auto duration = std::chrono::high_resolution_clock::now() - start;
+		auto seconds = std::chrono::duration_cast<std::chrono::duration<float>>(duration);
+		std::cout << "Time ist: " << 1000 * seconds.count() << " mseconds" << std::endl;
+	}
+private:
+	std::chrono::time_point<std::chrono::high_resolution_clock> start;
+};
+
+void PrintHello()
+{
+	Timer timer;
+	for (int i = 0; i < 100; i++)
+	{
+		std::cout << "Hello World!" << std::endl;
+	}
+}
+
 int main() 
 {
-	auto start = std::chrono::high_resolution_clock::now();
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	auto duration = std::chrono::high_resolution_clock::now() - start;
-	auto seconds = std::chrono::duration_cast<std::chrono::duration<float>>(duration);
-	std::cout << "Time ist: " << seconds.count() << " seconds" << std::endl;
+	PrintHello();
+
     return 0;
 }
