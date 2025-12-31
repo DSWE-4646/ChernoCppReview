20d7f35c863f17da76fb4958be4cdf075b870aeb
SiberianSledCat <dy464697573@gmail.com>
2025-12-31 14:25:55 +0800

    2种转ms方式，/n替代std::endl release模式下的时间消耗

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -11,8 +11,10 @@ struct Timer
 	~Timer()
 	{
 		auto duration = std::chrono::high_resolution_clock::now() - start;
-		auto seconds = std::chrono::duration_cast<std::chrono::duration<float>>(duration);
-		std::cout << "Time ist: " << 1000 * seconds.count() << " mseconds" << std::endl;
+		//auto seconds = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(duration);
+		auto seconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+		//std::cout << "Time ist: " << 1000 * seconds.count() << " mseconds" << std::endl;
+		std::cout << "Time ist: " <<seconds.count() << " mseconds" << std::endl;
 	}
 private:
 	std::chrono::time_point<std::chrono::high_resolution_clock> start;
@@ -23,6 +25,7 @@ void PrintHello()
 	Timer timer;
 	for (int i = 0; i < 100; i++)
 	{
+		//std::cout << "Hello World!\n";
 		std::cout << "Hello World!" << std::endl;
 	}
 }
