b96ad33232b3514dc680c2c0232b3d099acda7d4
SiberianSledCat <dy464697573@gmail.com>
2026-01-04 11:55:48 +0800

    静态2D数组以及auto不退化指针访问

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,38 +1,16 @@
 #include <iostream>
-#include<thread>
-#include<chrono>
-
-struct Timer
-{
-	Timer()
-	{
-		start = std::chrono::high_resolution_clock::now();
-	}
-	~Timer()
-	{
-		auto duration = std::chrono::high_resolution_clock::now() - start;
-		//auto seconds = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(duration);
-		auto seconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
-		//std::cout << "Time ist: " << 1000 * seconds.count() << " mseconds" << std::endl;
-		std::cout << "Time ist: " <<seconds.count() << " mseconds" << std::endl;
-	}
-private:
-	std::chrono::time_point<std::chrono::high_resolution_clock> start;
-};
-
-void PrintHello()
-{
-	Timer timer;
-	for (int i = 0; i < 100; i++)
-	{
-		//std::cout << "Hello World!\n";
-		std::cout << "Hello World!" << std::endl;
-	}
-}
 
 int main() 
 {
-	PrintHello();
+	int static_2D[5][5] = { {1, 2, 3, 4, 5},
+							{6, 7, 8, 9, 10},
+							{11, 12, 13, 14, 15},
+							{16, 17, 18, 19, 20},
+							{21, 22, 23, 24, 25} };
 
+	//for( int (&row)[5] : static_2D) auto实质就是这个对数组的引用，加括号
+	for (auto& row : static_2D)
+		for(auto elem : row)
+		std::cout << elem << std::endl;
     return 0;
 }
