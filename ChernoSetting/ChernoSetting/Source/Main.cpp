2c32c4aa60873d7449dac8b8a551bc939dc0cd0e
SiberianSledCat <dy464697573@gmail.com>
2026-01-05 11:43:51 +0800

    重载运算符使用默认升序排序自定义类型

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -6,14 +6,17 @@ struct Player
 {
 	std::string name;
 	int score;
+
+	bool operator<(const Player& other) const
+	{
+		return score < other.score; // Sort by score ascending
+	}
 };
 
 int main()
 {
 	std::vector<Player> players = { {"Alice", 80},{"Bob", 90} };
-	std::sort(players.begin(), players.end(), [](const Player& p1, const Player& p2) {
-		return p1.score > p2.score; // Sort by score descending
-		});
+	std::sort(players.begin(), players.end());
 
 	for (const auto& player : players)
 	{
