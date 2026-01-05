ab956261ab4e4d1e969d789c06adb6b0fcec5ddf
SiberianSledCat <dy464697573@gmail.com>
2026-01-05 11:34:42 +0800

    基本类型的sort排序

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
 #include <vector>
-
-class Grid2D
-{
-    int rows;
-    int cols;
-    std::vector<int> data;
-
-public:
-    Grid2D(int r, int c) :
-        rows(r), cols(c), data(r * c, 0) {}
-
-    int& operator() (int r, int c)
-    {
-        if (r < 0 || r >= rows || c < 0 || c >= cols) {
-            throw std::out_of_range("Index out of bounds");
-        }
-        return data[r * cols + c];
-    }
-    int getRows() const { return rows; }
-    int getCols() const { return cols; }
-
-};
+#include <algorithm>
+#include <functional>
 
 int main()
 {
-    Grid2D grid(3, 3);
-    grid(0, 0) = 1;
-    grid(0, 1) = 2;
+	std::vector<int> numbers = { 5, 3, 8, 1, 2 };
+	std::sort(numbers.begin(), numbers.end(), [](int a, int b) {
+		if (a == 1) return false;
+		if (b == 1) return true;
+		return a < b;
+		});
+	for (const auto& num : numbers)
+	{
+		std::cout << num << " ";
+	}
 }
