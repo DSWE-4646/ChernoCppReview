80b8f074708c05268abf932ae5f2dece707a93e8
SiberianSledCat <dy464697573@gmail.com>
2026-01-07 17:31:48 +0800

    匿名union与结构体、使用union使一个结构体可被多种视图使用

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
-
-struct Vector2
+struct Vec2
 {
-	int x, y;
+    float x, y;
 };
-
-/* 共享内存导致y出现类型双关中同样的垃圾值
-union Entity
+struct Vec4
 {
-		int x;
-		float y;
-};
-*/
+    union
+    {
+        struct
+        {
+            union { float x, r; };
+            union { float y, g; };
+            union { float z, b; };
+            union { float w, a; };
+        };
 
-struct Vector4
-{
-		int x, y, z, w;
-		Vector2& GetFront()
-		{
-			return *(Vector2*)&x;
-		}
+        struct {
+            Vec2 a;
+            Vec2 b;
+        }byVec2;
+    };
+    Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
 };
 
 int main()
 {
-	Vector4 e;
-	Vector2& f = e.GetFront();
-	e.x = 333;
-	e.y = 444;
-	std::cout << e.x << std::endl;
-	std::cout << e.y << std::endl;
-	f.x = 555;
-	f.y = 666;
-	std::cout << e.x << std::endl;
-	std::cout << e.y << std::endl;
-	
-	std::cin.get();
+    Vec4 vec(1.0f, 2.0f, 3.0f, 4.0f);
+    vec.z = 500.0f;
+    std::cout << vec.byVec2.b.x << std::endl; 
+    vec.g = 10.0f;
 }
