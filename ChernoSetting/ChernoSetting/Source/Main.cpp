4860e5fc459b51c85896d02b908bd41ea3902a68
SiberianSledCat <dy464697573@gmail.com>
2026-01-09 12:53:00 +0800

    虚析构1：基础情况

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
-struct Vec2
+class Base
 {
-    float x, y;
-};
-struct Vec4
-{
-    union
+public:
+    Base()
     {
-        struct
-        {
-            union { float x, r; };
-            union { float y, g; };
-            union { float z, b; };
-            union { float w, a; };
-        };
+        std::cout << "Base Constructor Called" << std::endl;
+    }
 
-        struct {
-            Vec2 a;
-            Vec2 b;
-        }byVec2;
-    };
-    Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+    ~Base()
+    {
+        std::cout << "Base Destructor Called" << std::endl;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    Derived()
+    {
+        std::cout << "Derived Constructor Called" << std::endl;
+    }
+    
+    ~Derived()
+    {
+        std::cout << "Derived Destructor Called" << std::endl;
+    }
 };
 
 int main()
 {
-    Vec4 vec(1.0f, 2.0f, 3.0f, 4.0f);
-    vec.z = 500.0f;
-    std::cout << vec.byVec2.b.x << std::endl; 
-    vec.g = 10.0f;
+    std::cout<<"This is Base ==============================="<<std::endl;
+    Base* base = new Base();
+    delete base;
+
+    std::cout<<"This is Derived ==============================="<<std::endl;
+    Derived* derived = new Derived();
+    delete derived;
+
+    std::cin.get();
+    return 0;
 }
