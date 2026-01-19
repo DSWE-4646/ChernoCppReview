e3194f0667b07a4b6a0354cb9980c08126970bac
SiberianSledCat <dy464697573@gmail.com>
2026-01-19 15:46:27 +0800

    结构绑定的特性

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,35 +1,40 @@
-#include <iostream>
-#include <tuple>
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <optional>
 
-std::tuple<std::string, int, std::string> WhatAmI()
+// 传统方案：返回空字符串表示失败
+std::optional<std::string> ReadFileAsString(const std::string& filepath) //1.添加Optional
 {
-    return std::make_tuple("SiberianSledCat", 24, "Computer Graphics");
+    std::ifstream file(filepath);
+    std::string result;
+    if (!file.is_open()) {
+        return {}; // 空字符串：无法区分“文件打开失败”和“文件本身为空”
+    }
+    // 读取文件内容到result...
+    file>>result;
+    file.close();
+    return result;
 }
 
-int main()
-{
-    /* C++17 新特性，结构绑定，方便快捷 */
-    auto [name, age, subject] = WhatAmI(); //C++17 Needed
-    std::cout << name << " is " << age << " years old and studies " << subject << std::endl;
-
-    /* 传统std::tuple 绑定，需要先声明tuple */
-    std::tuple<std::string, int, std::string> StudentEin =  WhatAmI();
-    std::cout << std::get<0>(StudentEin) << " is " << std::get<1>(StudentEin) << " years old and studies " << std::get<2>(StudentEin) << std::endl;
-
-    /* 传统std::tie 绑定，需要先声明变量 */
-    std::string name2;
-    int age2;
-    std::string subject2;
-    std::tie(name2, age2, subject2) = WhatAmI();
-    
+int main() {
+    //std::string data = ReadFileAsString("data.txt");
+    auto data = ReadFileAsString("data.txt");
+    //if (data.empty()) // 歧义：是文件打不开？还是文件本身是空的？
+    if (data) //2.检验Optional包裹的有效性，也可以使用data.has_value()
+    {
+        std::cout << "data opened and is not empty" << std::endl; //
+        std::cout << "The data is(*data):" << *data <<std::endl; //
+        std::cout << "The data is(data.value()):" << data.value() << std::endl; //
+        std::cout << "the data is(data.value_or)" << data.value_or("数据不存在") << std::endl; //带默认值的访问
+    }
+    else
+    {
+        std::cout << "文件打开失败" << std::endl;
+    }
+        
+    //std::cout << data << std::endl;
     std::cin.get();
     return 0;
 }
-
-
-
-
-
-
 
