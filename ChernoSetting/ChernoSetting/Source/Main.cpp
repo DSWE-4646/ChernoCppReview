48d7f26279f7db769d34714067eb2a644fe50a56
SiberianSledCat <dy464697573@gmail.com>
2026-01-29 12:40:09 +0800

    预编译头文件

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,32 +1,48 @@
-#include <string>
-#include <iostream>
-#include <variant>
+#include "pch.h"
+#include <string_view>
+
+static uint32_t s_AllocCount = 1;
+
+void* operator new(size_t size)
+{
+	s_AllocCount++;
+	std::cout << "Allocating " << size << " bytes\n";
+	return malloc(size);
+}
+
+void operator delete(void* ptr)
+{
+	s_AllocCount--;
+	std::cout << "Freeing memory\n";
+	free(ptr);
+}
+
+
+void PrintFirstName(std::string_view name)
+{
+	std::cout << "First name: " <<name << std::endl;
+}
+
+void PrintSecondName(std::string_view name)
+{
+	std::cout << "Second name: " <<name << std::endl;
+}
 
 int main()
 {
-    std::variant<int, std::string> data;
-    data = 123;
+	std::string fullname = "SiberianSledCat";
+	std::string_view FirstName(fullname.c_str(), 6);
+	std::string_view SecondName (fullname.c_str() + 7, 13);
+	PrintFirstName(FirstName);
+	PrintFirstName(FirstName);
+	//std::string FirstName = fullname.substr(0, 7);
+	//PrintFirstName(FirstName);
+	//PrintSecondName(fullname.substr(7, 13));
 
-    //按类型获取
-    std::cout<< "By Type:" << std::get<int>(data) << std::endl;
-    std::cout<< "Type Index is:" << data.index() << std::endl;
-    data = "Hello World";
-    std::cout << "By Type:" << std::get<std::string>(data) << std::endl;
-    std::cout << "Type Index is:" << data.index() << std::endl;
+	//PrintFirstName("SiberianSledCat");
+	//PrintSecondName("SledCat");
+	std::cout << "Allocations: " << s_AllocCount << std::endl;
 
-    //按索引获取
-    std::cout << "By Index:" << std::get<1>(data) << std::endl;
-
-    //返回指向的指针，类型不匹配返回nullptr
-    const std::string* ptr = std::get_if<std::string>(&data);
-    if (ptr)
-    {
-        std::cout << "By Pointer:" << *ptr << std::endl;
-    }
-
-    
-    
-    std::cin.get();
-    return 0;
+	std::cin.get();
+	return 0;
 }
-
