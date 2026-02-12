f2dd87948be62f305a4d00fbd06fdc48c4fc42fd
SiberianSledCat <dy464697573@gmail.com>
2026-02-12 14:09:43 +0800

    使用std::any存储任意类型以及查看32字节时的堆分配

--- ChernoSetting/ChernoSetting/Source/Main.cpp
+++ ChernoSetting/ChernoSetting/Source/Main.cpp
@@ -1,60 +1,55 @@
 #include <iostream>
+#include <any>
+#include <string>
 
-//饿汉式单例模式，线程安全，但不支持延迟初始化，程序启动时就创建实例
-/*
-class Singleton
+uint8_t AllocCount = 0;
+void* operator new(size_t size)
 {
-	//私有化默认构造函数，防止外部实例化，等效于私有空实现，是推荐写法
-	Singleton() = default;
-public:
+	AllocCount++;
+	std::cout << "Allocating " << size << " bytes" << std::endl;
+	return malloc(size);
+}
 
-	//删除拷贝构造函数和赋值运算符，防止复制实例。移动构造函数此时被编译器隐式删除
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
-
-	//显式删除移动构造函数和赋值运算符，防止移动实例
-	Singleton(const Singleton&&) = delete;
-	Singleton& operator=(const Singleton&&) = delete;
-
-	static const Singleton& Get()
-	{
-		return Instance;
-	}
-
-private:
-	//类内声明静态唯一实例
-	static Singleton Instance;
+struct SmallStorge
+{
+	char SmallData[32];
 };
 
-Singleton Singleton::Instance; //类外初始化,必须执行
-*/
-
-//懒汉式单例模式，支持延迟初始化，线程安全（C++11及以上标准）
-class Singleton
+struct BigStorge
 {
-	//私有化默认构造函数，防止外部实例化，等效于私有空实现，是推荐写法
-	Singleton() = default;
-public:
-
-	//删除拷贝构造函数和赋值运算符，防止复制实例。移动构造函数此时被编译器隐式删除
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
-
-	static const Singleton& Get()
-	{
-		//局部静态变量，延迟初始化，线程安全
-		static Singleton Instance;
-		return Instance;
-	}
-
-	void PrintHello() const
-	{
-		std::cout << "Hello Singleton!" << std::endl;
-	}
-
-	//频繁使用的成员函数，可以封装一个静态调用接口，避免每次都要获取实例
-	void TestPrintHello() const
-	{
-		Get().PrintHello();
-	}
+	char BigData[1024];
 };
+
+int main()
+{
+	std::any data;
+	data = 1135;
+	std::cout << "The Type is:" <<data.type().name() << ",";
+	std::cout << "The Value is:" <<std::any_cast<int>(data) << std::endl;
+
+	data = 3.1415926;
+	std::cout << "The Type is:" << data.type().name() << ",";
+	std::cout << "The Value is:" << std::any_cast<double>(data) << std::endl;
+
+	/*
+	data = std::string("LessThan15B");
+	std::cout << "The Type is:" << data.type().name() << ",";
+	std::cout << "The Value is:" << std::any_cast<std::string>(data) << std::endl;
+	*/
+
+	/*
+	try
+	{
+		std::cout << "The Type is:" << data.type().name() << ",";
+		std::cout << "No Type Check lead to exception: " << std::any_cast<int>(data) << std::endl;
+	}
+	catch (const std::bad_any_cast& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	*/
+
+	data = SmallStorge{};
+	data = BigStorge{};
+	
+}
